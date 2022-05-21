1bc11bf3a3a5300cfb3b81e06242b482ef9f48c6
quspari <arneshon@gmail.com>
2022-05-21 15:59:29 +1000

    add https

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/private.key'),
+    cert: fs.readFileSync('./cert/certificate.crt'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   //app.use(logger())
   app.enableCors();
   await app.listen(80);
